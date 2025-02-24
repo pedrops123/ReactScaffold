40e73b0dca08e529def548c67a41d05609f84044
unknown <pedro.furlan1304@hotmail.com>
2025-02-24 19:27:31 -0300

    Mudança criação de componentes dinamico por caminho em parametro

--- TestReactApp/README.md
+++ TestReactApp/README.md
@@ -6,3 +6,22 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+# para funcionar faça :
+
+  1 - Entrar dentro da pasta TestReactApp
+  2 - rodar npm install para instalar as dependencias
+  3 - Digitar no cmd dentro da pasta  TestReactApp o comando vite para inicializar a aplicação
+
+# PARA CRIAR UM NOVO COMPONENTE DIGITE O COMANDO :
+
+plop component
+
+# PARA CRIAR UMA NOVA PAGINA DIGITE O COMANDO :
+
+plop page
+
+# TEMPLATES DOS SCAFFOLDS
+
+/plop-templates
--- TestReactApp/plopfile.js
+++ TestReactApp/plopfile.js
@@ -2,28 +2,40 @@ export default function (plop) {
     // create your generators here
     plop.setGenerator('component', {
         description: 'Default component react for our project',
-        prompts: [{
+        prompts: [
+        {
+            type: 'input',
+            name: 'path',
+            message: 'Insira o caminho a criar o componente'
+        },
+        {
             type: 'input',
             name: 'name',
             message: 'Insira o nome do seu componente'
         }],
         actions: [{
             type: 'add',
-            path: 'src/Components/{{name}}/{{name}}.jsx',
+            path: '{{path}}/{{name}}/{{name}}.jsx',
             templateFile: 'plop-templates/component.hbs'
         }]
     });
 
     plop.setGenerator('page', {
         description: 'Default page react for our project',
-        prompts: [{
+        prompts: [
+        {
+            type: 'input',
+            name: 'path',
+            message: 'Insira o caminho a criar a pagina'
+        },
+        {
             type: 'input',
             name: 'pagename',
             message: 'Insira o nome da sua pagina'
         }],
         actions: [{
             type: 'add',
-            path: 'src/Pages/{{pagename}}/{{pagename}}.jsx',
+            path: '{{path}}/{{pagename}}/{{pagename}}.jsx',
             templateFile: 'plop-templates/page.hbs'
         }]
     });
